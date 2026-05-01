9b3e05958f10e6aea2fe6898493b11d0e6e9fc03
Luis Farinango <44455402+LuisFFarinangoT@users.noreply.github.com>
2026-04-30 23:00:17 -0500

    Change ouput to  router module

--- 02_Test_topology/modules/routers/naming-conventions/main.tf
+++ 02_Test_topology/modules/routers/naming-conventions/main.tf
@@ -2,7 +2,7 @@ locals {
   name_router = "${var.number}-${var.zone_identification}-${var.type_subgroup}"
 }
 
-output "workloads" {
+output "router_id_topo" {
   value = {
     router    = "R-${local.name_router}"
   }
