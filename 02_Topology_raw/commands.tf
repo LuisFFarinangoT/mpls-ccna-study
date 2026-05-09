78c51392e8996959b1c5e81f3c6f6e85e60dca4d
Luis Farinango <44455402+LuisFFarinangoT@users.noreply.github.com>
2026-05-09 00:22:57 -0500

    ports changed

--- 02_Topology_raw/commands.tf
+++ 02_Topology_raw/commands.tf
@@ -11,8 +11,7 @@ resource "gns3_start_all" "start_nodes" {
     gns3_template.router7,
     gns3_template.router8,
     gns3_template.router9,
-    gns3_link.r1_r4
-    /*
+    gns3_link.r1_r4,
     gns3_link.r2_r4,
     gns3_link.r2_r5,
     gns3_link.r3_r6,
@@ -24,6 +23,6 @@ resource "gns3_start_all" "start_nodes" {
     gns3_link.r6_r9,
     gns3_link.r7_r8,
     gns3_link.r8_r9,
-    */
+
   ]
 }
